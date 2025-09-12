a82fbfaf645670bc37d8fd5f813bca2a189ecb58
Jeremy <enofejeremiah87@gmail.com>
2025-09-12 16:20:03 +0100

    update quantity

--- frontend/src/components/CartItem.jsx
+++ frontend/src/components/CartItem.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { Minus, Plus, Trash } from 'lucide-react';
-import { useCartStore } from '../stores/useCartStore';
+import { useCartStore } from '../stores/useCartStore.js';
 
-const CartItem = (item) => {
-  const {removeFromcart, updateQuantity} = useCartStore()
+const CartItem = ({item}) => {
+  const { removeFromCart,updateQuantity } = useCartStore();
   return (
     <div className="rounded-lg border p-4 shadow-sm border-gray-700 bg-gray-800 md:p-6">
       <div className="space-y-4 md:items-center md:justify-between md:gap-6 md:space-y-0">
@@ -11,7 +11,7 @@ const CartItem = (item) => {
           <img className="h-20 md:h-32 rounded object-cover" src={item.image} />
         </div>
 
-        <label className="sr-only">Choose quantity</label>
+        <label className="sr-only">Choose quantity:</label>
         <div className="flex items-center justify-between md:order-3 md:justify-end">
           <div className="flex items-center gap-2">
             <button
@@ -32,15 +32,27 @@ const CartItem = (item) => {
           </div>
           <div className="text-end md:order-4 md:w-32">
             <p className="text-base font-bold text-emerald-400">
-              #{item.price}
+              #
+              {item.price.toLocaleString("en-NG", {
+                minimumFractionDigits: 0,
+              })}
             </p>
           </div>
         </div>
-        <div  className='w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md'>
-          <p className='text-base font-medium text-white hover:text-emerald-400 hover:underline'>{item.name}</p>
-          <p className='text-sm text-gray-400'>{item.description}</p>
+        <div className="w-full min-w-0 flex-1 space-y-4 md:order-2 md:max-w-md">
+          <p className="text-base font-medium text-white hover:text-emerald-400 hover:underline">
+            {item.name}
+          </p>
+          <p className="text-sm text-gray-400">{item.description}</p>
 
-          <div className=''></div>
+          <div className="flex items-center gap-4">
+            <button
+              className="inline-flex items-center text-sm font-medium text-red-400 hover:text-red-300 hover:underline"
+              onClick={() => removeFromCart(item._id)}
+            >
+              <Trash />
+            </button>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -4,6 +4,7 @@ import { motion } from 'framer-motion';
 import { ShoppingCart } from 'lucide-react';
 import CartItem from '../components/CartItem';
 import PeopleAlsoBought from '../components/PeopleAlsoBought';
+import { Link } from 'react-router-dom';
 
 
 const CartPage = () => {
@@ -37,6 +38,7 @@ export default CartPage;
 
 
 const EmptyCartUI = () => {
+  return(
     <motion.div 
       className="flex flex-col items-center justify-center space-y-4 py-16"
       initial={{ opacity: 0, y: 20}}
@@ -45,9 +47,10 @@ const EmptyCartUI = () => {
       >
         <ShoppingCart className='h-24 w-24 text-gray-300'/>
         <h3 className='text-2xl font-semibold'>Your cart is empty</h3>
-        <p className='tet-gray-400'>`Looks like you have't added anything to your cart yet`</p>
+        <p className='tet-gray-400'>Looks like you have't added anything to your cart yet</p>
         <Link className='mt-4 rounded-md bg-emerald-500 px-6 py-2 text-white transition-color hover:bg-emerald-600' to='/'>
           Start Shopping
         </Link>
       </motion.div>
+  )
 }
