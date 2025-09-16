ed8c8a616010f7cc945d78db429bc782cc730788
Jeremy <enofejeremiah87@gmail.com>
2025-09-16 11:14:11 +0100

    test

--- backend/server.js
+++ backend/server.js
@@ -42,7 +42,7 @@ app.use("/api/analytics", analyticsRoutes);
 if(process.env.NODE_ENV ==="production"){
    const buildPath = path.join(__dirname, "../frontend/build");
    app.use(express.static(buildPath));
-   app.get(/.*/, (req, res) => {
+   app.get("*", (req, res) => {
      res.sendFile(path.join(buildPath, "index.html"));
    });
 }
