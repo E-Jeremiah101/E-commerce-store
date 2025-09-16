2e0c9562918ae78a57b607b1dcfd686a27a3774c
Jeremy <enofejeremiah87@gmail.com>
2025-09-16 03:27:35 +0100

    refresh token

--- frontend/src/stores/useUserStore.js
+++ frontend/src/stores/useUserStore.js
@@ -58,6 +58,49 @@ export const useUserStore = create((set, get) => ({
       set({ checkingAuth: false, user: null });
     }
   },
+  refreshToken: async () => {
+    if(get().checkingAuth) return;
+
+    set({checkingAuth: true});
+    try {
+      const response= await axios.post("/auth/refresh-token");
+      set({checkingAuth: false});
+      return response.data;
+    } catch (error) {
+      set({user: null, checkingAuth: false});
+      throw error;
+    }
+  }
 }));
 
 //Implementing the axios interceptors for refreshing the access tokens
+
+//Axios interceptor for token refresh
+
+let refreshPromise = null;
+axios.interceptors.response.use(
+  (response) => response,
+  async () => {
+    const originalRequest = error.config;
+    if (error.response?.status === 401 && !originalRequest._retry){
+      originalRequest._retry = true;
+
+      try {
+        if(refreshPromise){
+          await refreshPromise;
+          return axios(originalRequest);
+        }
+
+        refreshPromise = useUserStore.getState().refreshToken();
+        await refreshPromise;
+        refreshPromise = null;
+
+        return axios(originalRequest);
+      } catch (error) {
+        useUserStore.getState().logout();
+        return Promise.reject(refreshError);
+      }
+    }
+    return Promise.reject(error)
+  }
+);
